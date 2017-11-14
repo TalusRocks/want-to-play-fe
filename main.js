99ae285bf908f93c84867f0cdcb499e2d0170ccd
RJ <jensenrs@gmail.com>
2017-11-14 13:43:14 -0800

    created edit view, grabbing all fields except radio interest

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ const gamesURL = 'http://localhost:3000/games'
 let gameList = document.querySelector('.game-list')
 let allButtons = document.querySelector('.all-buttons')
 
-function goHome(gamesURL){
-  gameList.innerHTML = ""
-  loadGames(gamesURL)
-  allButtons.classList.remove('hide')
+/////////GENERAL USE
+function cancelAndGoHome(gamesURL) {
+  let cancel = document.querySelectorAll('.cancel')
+  cancel.forEach(e => {
+    e.addEventListener('click', (e) => {
+      gameList.innerHTML = ""
+      loadGames(gamesURL)
+      allButtons.classList.remove('hide')
+    })
+  })
 }
+
 
 //////////LOAD ALL GAMES
 function loadGames(gamesURL) {
@@ -14,9 +21,17 @@ function loadGames(gamesURL) {
     .then(result => {
       let games = result.data
       games.forEach(e => {
-        gameList.innerHTML += gameItem(e.interest, e.minPlayer, e.maxPlayer, e.minTime, e.maxTime, e.ratingBGG, e.weightBGG, e.name, e.notes, e.id, e.tags)
+        gameList.innerHTML += gameItem(e.name, e.interest, e.minPlayer, e.maxPlayer, e.minTime, e.maxTime, e.ratingBGG, e.weightBGG, e.notes, e.id, e.tags)
       })
       colorRanges()
+
+      let gameEditLink = document.querySelectorAll('.title')
+      for (let i = 0; i < gameEditLink.length; i++) {
+        gameEditLink[i].addEventListener('click', (e) => {
+          let thisData = result.data[i]
+          editGame(thisData.name, thisData.interest, thisData.minPlayer, thisData.maxPlayer, thisData.minTime, thisData.maxTime, thisData.ratingBGG, thisData.weightBGG, thisData.notes, thisData.gameId, thisData.tags, gamesURL)
+        })
+      }
     })
     .catch(errors => {
       console.log(errors);
@@ -24,7 +39,8 @@ function loadGames(gamesURL) {
 }
 loadGames(gamesURL)
 
-///////////ADD GAME
+
+///////////CREATE GAME
 let addGameBtn = document.querySelector('.add-game')
 addGameBtn.addEventListener('click', (e) => {
   //LOAD VIEW
@@ -35,29 +51,30 @@ addGameBtn.addEventListener('click', (e) => {
   let submit = document.querySelector('#submit')
   submit.addEventListener('click', (e) => {
     e.preventDefault()
-    let newName = document.querySelector('#game-name-input').value
-    let newInterest
+    let name = document.querySelector('#game-name-input').value
+    let interest
     let interestRange = document.getElementsByName('interest')
     for (let i = 0; i < interestRange.length; i++) {
       if (interestRange[i].checked) {
-        newInterest = interestRange[i].value
-        // return newInterest
+        interest = interestRange[i].value
         break;
       }
     }
+    let minPlayer = document.querySelector('#player-min-input').value
+    let maxPlayer = document.querySelector('#player-max-input').value
+    let minTime = document.querySelector('#time-min-input').value
+    let maxTime = document.querySelector('#time-max-input').value
+    let ratingBGG = document.querySelector('#rating-input').value
+    let weightBGG = document.querySelector('#weight-input').value
+    let notes = document.querySelector('#notes-input').value
 
-    let newMinPlayer = document.querySelector('#player-min-input').value
-    let newMaxPlayer = document.querySelector('#player-max-input').value
-    let newMinTime = document.querySelector('#time-min-input').value
-    let newMaxTime = document.querySelector('#time-max-input').value
-    let newRatingBGG = document.querySelector('#rating-input').value
-    let newWeightBGG = document.querySelector('#weight-input').value
-    let newNotes = document.querySelector('#notes-input').value
-
-    axios.post(gamesURL, {name: newName, interest: newInterest, minPlayer: newMinPlayer, maxPlayer: newMaxPlayer, minTime: newMinTime, maxTime: newMaxTime, ratingBGG: newRatingBGG, weightBGG: newWeightBGG, notes: newNotes})
+    axios.post(gamesURL, {name, interest, minPlayer, maxPlayer, minTime, maxTime, ratingBGG, weightBGG, notes})
       .then(result => {
         goHome(gamesURL)
-        console.log(result.data);
+      })
+      .then(result => {
+        //not redirecting??
+        goToAnchor(gamesURL, name)
       })
       .catch(errors => {
         console.log(errors);
@@ -65,12 +82,24 @@ addGameBtn.addEventListener('click', (e) => {
   })
 
   //CANCEL
-  let cancel = document.querySelectorAll('.cancel')
-  cancel.forEach(e => {
-    e.addEventListener('click', (e) => {
-      goHome(gamesURL)
-    })
-  })
-
+  cancelAndGoHome(gamesURL)
 
 })
+
+function goToAnchor(gamesURL, name) {
+  return window.location.href = `http://127.0.0.1:8080/#${name}`;
+}
+
+///////////EDIT GAME
+function editGame(name, interest, minPlayer, maxPlayer, minTime, maxTime, ratingBGG, weightBGG, notes, gameId, tags, gamesURL) {
+
+  //LOAD VIEW
+  gameList.innerHTML = editGameView(name, interest, minPlayer, maxPlayer, minTime, maxTime, ratingBGG, weightBGG, notes, gameId, tags, gamesURL)
+  allButtons.classList.add('hide')
+
+  //listen for submit
+  //axios put route
+
+  //CANCEL
+  cancelAndGoHome(gamesURL)
+}
--- templates.js
+++ templates.js
@@ -1,5 +1,6 @@
-function gameItem(interest, minPlayer, maxPlayer, minTime, maxTime, ratingBGG, weightBGG, name, notes, gameId, tags) {
-  return `<section class="game-item">
+function gameItem(name, interest, minPlayer, maxPlayer, minTime, maxTime, ratingBGG, weightBGG, notes, gameId, tags) {
+  return `<section class="game-item" data-id="${gameId}">
+          <a name="${name}"></a>
             <div class="game-specs">
               <div class="interest">
                 <p class="spec-number">${interest}</p>
@@ -23,7 +24,7 @@ function gameItem(interest, minPlayer, maxPlayer, minTime, maxTime, ratingBGG, w
               </div>
             </div>
             <div class="game-title-notes-tags">
-              <h2 class="title">${name}</h2>
+              <h2 class="title"><a href="#">${name}</a></h2>
               <p class="notes">${notes}</p>
               <div class="game-tags" data-id="${gameId}">
               ${populateTags(tags)}
@@ -39,6 +40,7 @@ function populateTags (tagArr) {
 function createTag(tagId, tagName) {
   return `<p class="tag" data-id="${tagId}"><a href="#">${tagName}</a></p>`
 }
+
 
 function addGameView() {
   return `        <section class="form-wrap mb-5">
@@ -140,6 +142,117 @@ function addGameView() {
       <div class="form-row mt-2">
         <label for="tags" id="tags">Tags (comma, separated)</label>
         <input type="text" name="tags" class="mt-05">
+      </div>
+
+      <div class="form-row cancel-save mt-2">
+        <button id="cancel" class="cancel">Cancel</button>
+        <button type="submit" id="submit">Save</button>
+      </div>
+
+    </form>
+  </section>`
+}
+
+function editGameView(name, interest, minPlayer, maxPlayer, minTime, maxTime, ratingBGG, weightBGG, notes, gameId, tags) {
+  return `<section class="form-wrap mb-5" data-id="${gameId}">
+    <div class="close-view">
+      <i class="material-icons close-x cancel">close</i>
+    </div>
+    <h2 class="mt-05 text-center">Edit Game</h2>
+    <form>
+
+      <div class="form-row mt-2">
+        <label for="game-name" id="game-name">Game name</label>
+        <input type="text" id="game-name-input" name="game-name" class="mt-05" value="${name}">
+      </div>
+
+      <div class="form-row mt-2">
+        <p>Level of interest</p><br>
+        <div class="radio-row">
+          <div>
+            <input type="radio" name="interest" id="interest-1" value="1">
+            <label for="interest-1"><span>1</span></label>
+          </div>
+          <div>
+            <input type="radio" name="interest" id="interest-2" value="2">
+            <label for="interest-2">2</label>
+          </div>
+          <div>
+            <input type="radio" name="interest" id="interest-3" value="3">
+            <label for="interest-3">3</label>
+          </div>
+          <div>
+            <input type="radio" name="interest" id="interest-4" value="4">
+            <label for="interest-4">4</label>
+          </div>
+          <div>
+            <input type="radio" name="interest" id="interest-5" value="5">
+            <label for="interest-5">5</label>
+          </div>
+          <div>
+            <input type="radio" name="interest" id="interest-6" value="6">
+            <label for="interest-6">6</label>
+          </div>
+          <div>
+            <input type="radio" name="interest" id="interest-7" value="7">
+            <label for="interest-7">7</label>
+          </div>
+          <div>
+            <input type="radio" name="interest" id="interest-8" value="8">
+            <label for="interest-8">8</label>
+          </div>
+          <div>
+            <input type="radio" name="interest" id="interest-9" value="9">
+            <label for="interest-9">9</label>
+          </div>
+          <div>
+            <input type="radio" name="interest" id="interest-10" value="10">
+            <label for="interest-10">10</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-row two-input-row">
+        <div class="half-form-row">
+          <label for="player-min" id="player-min">Player min</label>
+          <input type="text" id="player-min-input" name="player-min" class="mt-05" value="${minPlayer}">
+        </div>
+        <div class="half-form-row">
+          <label for="player-max" id="player-max">Player max</label>
+          <input type="text" id="player-max-input" name="player-max" class="mt-05" value="${maxPlayer}">
+        </div>
+      </div>
+
+      <div class="form-row two-input-row mt-2">
+        <div class="half-form-row">
+          <label for="time-min" id="time-min">Time min (minutes)</label>
+          <input type="text" id="time-min-input" name="time-min" class="mt-05" value="${minTime}">
+        </div>
+        <div class="half-form-row">
+          <label for="time-max" id="time-max">Time max (minutes)</label>
+          <input type="text" id="time-max-input" name="time-max" class="mt-05" value="${maxTime}">
+        </div>
+      </div>
+
+      <div class="form-row two-input-row mt-2">
+        <div class="half-form-row">
+          <label for="rating" id="rating">Rating (1-10)</label>
+          <input type="text" id="rating-input" name="rating" class="mt-05" value=${ratingBGG}>
+        </div>
+        <div class="half-form-row">
+          <label for="weight" id="weight">Weight (1-5)</label>
+          <input type="text" id="weight-input" name="weight" class="mt-05" value="${weightBGG}">
+        </div>
+      </div>
+
+      <div class="form-row mt-2">
+        <label for="notes" id="notes">Notes</label>
+        <textarea name="notes" id="notes-input" rows="4" class="mt-05" value="${notes}"></textarea>
+      </div>
+
+      <div class="form-row mt-2">
+        <label for="tags" id="tags">Tags (comma, separated)</label>
+        <input type="text" name="tags" class="mt-05" value="${tags}">
       </div>
 
       <div class="form-row cancel-save mt-2">

7a5c0e05e1abe6234648c310bd9c437aa0a7c078
RJ <jensenrs@gmail.com>
2017-11-15 15:35:51 -0800

    using links instead of checkboxes, and sort options now populate in and out of sort by div

--- sort.js
+++ sort.js
@@ -1,58 +1,38 @@
 //wrap in a function to fire when sort is clicked (do that in main.js)
 
-//in THENBY listen for click on checkbox (or "checked?"),
-//remove from THENBY and move to SORTBY (checked)
-let checkboxInputs = document.querySelectorAll('input[name="sortby"]')
+let sortByButton = document.querySelectorAll('.sortby')
 
-let sortByDiv = document.querySelector('.all-sortby-checkboxes')
-let thenByDiv = document.querySelector('.all-thenby-checkboxes')
+let selectedArr = []
 
 
-
-function isChecked() {
-  let sortByArr = []
-  let thenByArr = []
-  checkboxInputs.forEach(e => {
-    // console.log(e);
-    if(e.checked){
-      sortByArr.push(e.value)
+sortByButton.forEach(el => {
+  el.addEventListener('click', (evt) => {
+    let sortItem = evt.srcElement
+    //if it is to BE selected..
+    if (!sortItem.classList.contains('selected')) {
+      sortItem.classList.add('selected')
+      let sortObj = {id: sortItem.id, text: sortItem.textContent}
+      selectedArr.push(sortObj)
+    } else {
+      //if it is to be UNselected..
+      sortItem.classList.remove('selected')
+      //where id matches, remove from selected array
+      selectedArr.forEach(e => {
+        if (e.id === sortItem.id) {
+          let index = selectedArr.indexOf(e)
+          selectedArr.splice(index, 1)
+        }
+      })
     }
-    console.log(sortByArr);
-  })
-
-}
-
-let checkboxes = document.querySelectorAll('.sort-var')
-
-checkboxes.forEach(e => {
-  e.addEventListener('click', (e) => {
-    e.preventDefault()
-    const label = e.target.getAttribute('for')
-    document.querySelector(`#${label}`).checked = true
-    isChecked()
+    loadSelected(selectedArr)
   })
 })
 
+let sortByDiv = document.querySelector('.all-sortby')
+let thenByDiv = document.querySelector('.all-thenby')
 
-// for (let i = 0; i < checkboxes.length; i++) {
-//   checkboxes[i].addEventListener('click', (e) => {
-//     console.log("clicko");
-//
-//     // isChecked()
-//   })
-// }
-
-
-
-
-//${interest == '1' ? 'checked' : ''}
-
-//in SORTBY listen for click on checkbox,
-//remove from SORTBY and move to THENBY (unchecked)
-
-//on sort button, listen for click
-//send an ordered list to the load games function...
-
-//then get in that load games function and sort
-
-//...or these don't have to be checkboxes- could just be links..
+function loadSelected(selectedArr) {
+  sortByDiv.innerHTML = ""
+  for(let i = 0; i < selectedArr.length; i++)
+    sortByDiv.innerHTML += `<p class="sortby mtb-1" id="${selectedArr[i].id}">${selectedArr[i].text}</p>`
+}
